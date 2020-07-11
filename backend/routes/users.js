59c418013a3999895fb676136141bf8224817d81
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-07-11 16:14:15 +0200

    exercise delete and update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// get
 router.route('/').get((req, res) => {
     User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err))
 });
 
+// add
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const newUser = new User({ username });
