3f6de271ad04f03c599b94af80b69c7ca2f4b1c8
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-07-11 15:44:01 +0200

    read and write

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose'),
+const mongoose = require('mongoose');
+const Schema =  mongoose.Schema;
 
-const Schemaa = mongoose.Schema;
 
 const excerciseSchema = new Schema({
     username: { type: String, required: true },
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors =  require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
+const exercisesRouter = require('./routes/exercises.js');
+const usersRouter = require('./routes/users');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,11 +12,14 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
-const connection = mongoose.connection;
-connection.once('open', () => {
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
     console.log("MongoDB database connection establihed successfully");
 })
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
